802389dc9bef83c4998cb776db635ce1367c5846
martynakulis <kulismartyna@gmail.com>
2025-02-06 12:52:33 +0100

    change

--- src/redux/store.js
+++ src/redux/store.js
@@ -24,7 +24,7 @@ const authPersistConfig = {
 export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authReducer),
-    tasks: contactsReducer,
+    contacts: contactsReducer,
     filter: filterReducer,
   },
   middleware: getDefaultMiddleware =>
